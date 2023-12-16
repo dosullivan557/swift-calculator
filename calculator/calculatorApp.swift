99d27d458af0b7de81624b31539606a239a135eb
Danny O'Sullivan <dosullivan557@gmail.com>
2023-12-16 18:34:10 +0000

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Danny on 16/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
